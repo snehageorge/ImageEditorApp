172666babe7430844c62b926176278d995461050
Sneha <snehavg94@gmail.com>
2025-04-16 15:27:32 +0200

    Initial Commit

---
+++ ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by Sneha on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
