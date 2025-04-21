729bdc32210db268615552bfcded6f6e434d9a98
Sneha <snehavg94@gmail.com>
2025-04-21 13:05:15 +0200

    Added image filters

---
+++ ImageEditor/PhotoEditorViewModel.swift
@@ -0,0 +1,117 @@
+//
+//  PhotoEditorViewModel.swift
+//  ImageEditor
+//
+//  Created by Sneha on 21/04/25.
+//
+
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+class PhotoEditorViewModel: ObservableObject {
+    
+    @Published var originalImage: UIImage?
+    @Published var editedImage: UIImage?
+    @Published var selectedFilter: FilterType = .none
+        
+    var brightnessValue: Float = 0.0 // Range: -1.0 to 1.0
+    var contrastValue: Float = 1.0  // Range: 0 to 4.0
+    
+    func setOriginalImage(_ image: UIImage) {
+        self.originalImage = image
+        resetImage()
+    }
+    
+    func selectFilter(_ type: FilterType) {
+        selectedFilter = type
+        applyFilter()
+    }
+    
+    func updateBrightness(_ value: Float) {
+        brightnessValue = value
+        applyFilter()
+    }
+    
+    func updateContrast(_ value: Float) {
+        contrastValue = value
+        applyFilter()
+    }
+    
+    func resetImage() {
+        selectedFilter = .none
+        brightnessValue = 0.0
+        contrastValue = 1.0
+        applyFilter()
+    }
+    
+    func applyFilter() {
+        guard let originalImage = originalImage,
+              let inputCIImage = CIImage(image: originalImage) else {
+            return
+        }
+        
+        let filteredCIImage: CIImage? = {
+            switch selectedFilter {
+            case .none:
+                return inputCIImage
+            case .blackAndWhite:
+                let filter = CIFilter.photoEffectMono()
+                filter.inputImage = inputCIImage
+                return filter.outputImage
+            case .sepia:
+                let filter = CIFilter.sepiaTone()
+                filter.intensity = 1.0
+                filter.inputImage = inputCIImage
+                return filter.outputImage
+            case .monochrome:
+                let filter = CIFilter.colorMonochrome()
+                filter.intensity = 1.0
+                filter.color = CIColor(red: 0.7, green: 0.7, blue: 0.7) //grey tone
+                filter.inputImage = inputCIImage
+                return filter.outputImage
+            case .noir:
+                let filter = CIFilter.photoEffectNoir()
+                filter.inputImage = inputCIImage
+                return filter.outputImage
+            case .vignette:
+                let filter = CIFilter.vignette()
+                filter.intensity = 2.0
+                filter.radius = 3.0
+                filter.inputImage = inputCIImage
+                return filter.outputImage
+            }
+        }()
+        
+        guard let baseImageForAdjustments = filteredCIImage else { return }
+        
+        // Apply brightness and contrast
+        let adjustFilter = CIFilter.colorControls()
+        adjustFilter.inputImage = baseImageForAdjustments
+        adjustFilter.brightness = brightnessValue
+        adjustFilter.contrast = contrastValue
+        
+        if let finalImage = adjustFilter.outputImage,
+           let cgImage = CIContext().createCGImage(finalImage, from: finalImage.extent) {
+            DispatchQueue.main.async {
+                self.editedImage = UIImage(cgImage: cgImage)
+            }
+        }
+    }
+}
+
+enum FilterType: String, CaseIterable, Identifiable {
+    case none, blackAndWhite, sepia, monochrome, noir, vignette
+    
+    var id: String { self.rawValue }
+    
+    var displayName: String {
+        switch self {
+        case .none: return "None"
+        case .blackAndWhite: return "B&W"
+        case .sepia: return "Sepia"
+        case .monochrome: return "Mono"
+        case .noir: return "Noir"
+        case .vignette: return "Vignette"
+        }
+    }
+}
---
+++ ImageEditor/PhotoFilterEditorView.swift
@@ -0,0 +1,89 @@
+//
+//  PhotoFilterEditorView.swift
+//  ImageEditor
+//
+//  Created by Sneha on 16/04/25.
+//
+
+import SwiftUI
+import PhotosUI
+import CoreImage.CIFilterBuiltins
+
+struct PhotoFilterEditorView: View {
+    @StateObject private var viewModel = PhotoEditorViewModel()
+    
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var brightness: Float = 0.0
+    @State private var contrast: Float = 1.0
+    
+    var body: some View {
+        VStack {
+            if let editedImage = viewModel.editedImage {
+                Image(uiImage: editedImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 300)
+                    .padding()
+            } else {
+                Rectangle()
+                    .fill(Color.gray.opacity(0.2))
+                    .frame(height: 300)
+                    .overlay(Text("No photo selected"))
+            }
+            
+            PhotosPicker("Select a Photo", selection: $selectedItem, matching: .images)
+                .padding()
+            
+            if let _ = viewModel.editedImage {
+                HStack {
+                    Text("Brightness")
+                    Slider(value: $brightness, in: -1...1)
+                        .onChange(of: brightness) { _, newValue in
+                            viewModel.updateBrightness(newValue)
+                        }
+                }
+                .padding()
+                
+                HStack {
+                    Text("Contrast   ")
+                    Slider(value: $contrast, in: 0...4)
+                        .onChange(of: contrast) { _, newValue in
+                            viewModel.updateContrast(newValue)
+                        }
+                }
+                .padding()
+                
+                Picker("Filter", selection: $viewModel.selectedFilter) {
+                    ForEach(FilterType.allCases) { filter in
+                        Text(filter.displayName).tag(filter)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                .onChange(of: viewModel.selectedFilter) { _, newValue in
+                    viewModel.selectFilter(newValue)
+                }
+            }
+        }
+        .onChange(of: selectedItem) { _, newItem in
+            Task {
+                if let item = newItem, let data = try? await item.loadTransferable(type: Data.self),
+                   let uiImage = UIImage(data: data) {
+                    viewModel.originalImage = uiImage
+                    viewModel.editedImage = nil
+                    brightness = 0
+                    contrast = 1.0
+                    viewModel.resetImage()
+                }
+            }
+        }
+        .onAppear() {
+            viewModel.setOriginalImage(UIImage())
+        }
+        
+    }
+}
+
+#Preview {
+    PhotoFilterEditorView()
+}
